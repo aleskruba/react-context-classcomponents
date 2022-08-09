b67561902f1cdef869aa36c4953f95098a14ca99
Ales <aleskruba@seznam.cz>
2022-08-09 16:04:30 +0200

    Commit message

---
+++ src/context/AuthContext.js
@@ -0,0 +1,26 @@
+import { toHaveFocus } from '@testing-library/jest-dom/dist/matchers';
+import React, {Component,createContext} from 'react';
+
+export const AuthContext = createContext()
+
+class AuthContextProvider extends Component {
+    state = { 
+        isAuthenticated : false
+     }
+     
+     toggleAuth = () => {
+        this.setState({isAuthenticated : !this.state.isAuthenticated})
+
+     }
+     
+    render() { 
+        return (
+            <AuthContext.Provider value={{...this.state, toggleAuth: this.toggleAuth }} >
+            {this.props.children}
+            </AuthContext.Provider>
+
+        );
+    }
+}
+ 
+export default AuthContextProvider;
